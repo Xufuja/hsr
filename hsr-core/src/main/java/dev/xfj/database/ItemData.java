2860065119555bdeb06bce54d07a21f6a0efaf3a
Xufuja <xufuja@gmail.com>
2023-09-22 22:47:58 +0200

    Not sure why I was passing the class separately when I can do .getClass()

--- hsr-core/src/main/java/dev/xfj/database/ItemData.java
+++ hsr-core/src/main/java/dev/xfj/database/ItemData.java
@@ -32,7 +32,7 @@ public class ItemData {
         Map<Integer, Item> items = new HashMap<>();
 
         for (Map.Entry<String, ItemConfigJson> entry : itemConfig.entrySet()) {
-            ItemNormal item = createBaseItem(ItemNormal.class, ItemConfigJson.class, entry.getValue());
+            ItemNormal item = createBaseItem(ItemNormal.class, entry.getValue());
             item.setPurposeType(entry.getValue().getPurposeType());
             items.put(item.getItemId(), item);
         }
@@ -44,7 +44,7 @@ public class ItemData {
         Map<Integer, ItemEquipment> items = new HashMap<>();
 
         for (Map.Entry<String, ItemConfigEquipmentJson> entry : itemConfigEquipment.entrySet()) {
-            ItemEquipment item = createBaseItem(ItemEquipment.class, ItemConfigEquipmentJson.class, entry.getValue());
+            ItemEquipment item = createBaseItem(ItemEquipment.class, entry.getValue());
             item.setSellable(entry.getValue().isIsSellable());
             List<ItemCount> returns = new ArrayList<>();
 
@@ -65,7 +65,7 @@ public class ItemData {
         Map<Integer, ItemExp> items = new HashMap<>();
 
         for (String key : EquipmentData.getEquipmentExpItemConfigJson().keySet()) {
-            ItemExp item = createBaseItem(ItemExp.class, ItemConfigJson.class, itemConfig.get(key));
+            ItemExp item = createBaseItem(ItemExp.class, itemConfig.get(key));
             item.setExpProvide(EquipmentData.getEquipmentExpItemConfigJson().get(key).getExpProvide());
             item.setCoinCost(EquipmentData.getEquipmentExpItemConfigJson().get(key).getCoinCost());
             items.put(item.getItemId(), item);
@@ -74,26 +74,26 @@ public class ItemData {
         return items;
     }
 
-    private static <T, U> U createBaseItem(Class<U> returnType, Class<T> clazz, Object itemConfig) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
-        Constructor<U> constructor = returnType.getConstructor();
+    private static <T> T createBaseItem(Class<T> returnType, Object itemConfig) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
+        Constructor<T> constructor = returnType.getConstructor();
         Object item = constructor.newInstance();
         //I suppose it works...
-        item.getClass().getMethod("setItemId", int.class).invoke(item, clazz.getMethod("getId").invoke(itemConfig));
-        item.getClass().getMethod("setMainType", String.class).invoke(item, clazz.getMethod("getItemMainType").invoke(itemConfig));
-        item.getClass().getMethod("setSubType", String.class).invoke(item, clazz.getMethod("getItemSubType").invoke(itemConfig));
-        item.getClass().getMethod("setInventoryDisplayTag", int.class).invoke(item, clazz.getMethod("getInventoryDisplayTag").invoke(itemConfig));
-        item.getClass().getMethod("setRarity", String.class).invoke(item, clazz.getMethod("getRarity").invoke(itemConfig));
+        item.getClass().getMethod("setItemId", int.class).invoke(item, itemConfig.getClass().getMethod("getId").invoke(itemConfig));
+        item.getClass().getMethod("setMainType", String.class).invoke(item, itemConfig.getClass().getMethod("getItemMainType").invoke(itemConfig));
+        item.getClass().getMethod("setSubType", String.class).invoke(item, itemConfig.getClass().getMethod("getItemSubType").invoke(itemConfig));
+        item.getClass().getMethod("setInventoryDisplayTag", int.class).invoke(item, itemConfig.getClass().getMethod("getInventoryDisplayTag").invoke(itemConfig));
+        item.getClass().getMethod("setRarity", String.class).invoke(item, itemConfig.getClass().getMethod("getRarity").invoke(itemConfig));
 
-        Object name = clazz.getMethod("getItemName").invoke(itemConfig);
+        Object name = itemConfig.getClass().getMethod("getItemName").invoke(itemConfig);
         item.getClass().getMethod("setName", String.class).invoke(item, TextMapData.getTranslation((Integer) name.getClass().getMethod("getHash").invoke(name)));
 
-        Object bg = clazz.getMethod("getItemBGDesc").invoke(itemConfig);
+        Object bg = itemConfig.getClass().getMethod("getItemBGDesc").invoke(itemConfig);
         item.getClass().getMethod("setBackgroundDescription", String.class).invoke(item, TextMapData.getTranslation((Integer) bg.getClass().getMethod("getHash").invoke(bg)));
 
-        Object desc = clazz.getMethod("getItemDesc").invoke(itemConfig);
+        Object desc = itemConfig.getClass().getMethod("getItemDesc").invoke(itemConfig);
         item.getClass().getMethod("setDescription", String.class).invoke(item, TextMapData.getTranslation((Integer) desc.getClass().getMethod("getHash").invoke(desc)));
 
-        item.getClass().getMethod("setStackLimit", int.class).invoke(item, clazz.getMethod("getPileLimit").invoke(itemConfig));
+        item.getClass().getMethod("setStackLimit", int.class).invoke(item, itemConfig.getClass().getMethod("getPileLimit").invoke(itemConfig));
 
         return returnType.cast(item);
     }
