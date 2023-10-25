66f92242415a4a209612cbd12abc66dbedec2c08
Xufuja <xufuja@gmail.com>
2023-10-25 21:43:48 +0200

    Added start of Player class

---
+++ hsr-core/src/main/java/dev/xfj/player/Player.java
@@ -0,0 +1,11 @@
+package dev.xfj.player;
+
+import dev.xfj.character.Character;
+
+import java.util.List;
+
+public class Player {
+    private int trailBlazeLevel;
+    private int equilibriumLevel;
+    private List<Character> characters;
+}
